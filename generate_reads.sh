ef8eea96b5141ec0a685418d4e1dc8549ec588f8
Arun Das <adas21@jhu.edu>
2020-10-29 18:23:38 -0400

    Read Generation Script

---
+++ generate_reads.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+JVD=$1
+GNMS=$2
+RDS=$3
+SNP=$4
+INS=$5
+DEL=$6
+MLN=$7
+CVG=$8
+javac $JVD/*.java
+for i in $GNMS/*; do
+    if [ "${i}" != "${i%.fasta}" ] || [ "${i}" != "${i%.fna}" ] || [ "${i}" != "${i%.fa}" ];then
+        GNM=${i##*/}
+        echo "Generating Reads for ${GNM%%.*}"
+        java -cp $JVD ReadSimulator ${i} $RDS/${GNM%%.*}.fasta $SNP $INS $DEL $MLN $CVG
+    fi
+done
+echo "All Reads Generated!"
